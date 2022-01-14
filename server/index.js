fef375d2931dde74aa5cca183f1459e53c404e2c
Vincent Kranendonk <vincent@studiorabota.com>
2022-01-14 10:41:56 +0100

    Seperated client and server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = new Server(server, {  
+  cors: {    
+    origin: "http://localhost:5500",    
+    methods: ["GET", "POST"]  
+  }
 });
 
 io.on('connection', (socket) => {
@@ -14,17 +15,13 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
-});
 
-io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+      console.log('message: ' + msg);
     });
-});
 
-io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+      io.sockets.emit('chat message', msg);
     });
 });
 
