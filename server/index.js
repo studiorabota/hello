fa27eedc26e017f2c6e84ce71cb456b778eae11c
Vincent Kranendonk <vincent@studiorabota.com>
2022-01-14 11:46:55 +0100

    Removed test start of server on Heroku

--- server/index.js
+++ server/index.js
@@ -10,10 +10,6 @@ const io = new Server(server, {
   }
 });
 
-app.get('/', (req, res) => {  
-  res.send('<h1>Hello world</h1>');
-});
-
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
